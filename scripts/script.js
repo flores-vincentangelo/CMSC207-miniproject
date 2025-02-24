ff7426462e039e079bab8b7a8ac45bddfc99a014
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2025-02-24 14:58:49 +0800

    updates

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () { 
+    $(".section-buttons").click(function (e) { 
+        e.preventDefault();
+    });
+
+    $("#am-button").click(function (e) { 
+        console.log("clicked");
+
+        $("#am-container").css("display", "block");
+        $("#hni-container").css("display", "none");
+        $("#ci-container").css("display", "none");
+    });
+    
+    $("#hni-button").click(function (e) { 
+        console.log("clicked");
+
+        $("#am-container").css("display", "none");
+        $("#hni-container").css("display", "block");
+        $("#ci-container").css("display", "none");
+    });
+
+    $("#ci-button").click(function (e) { 
+        console.log("clicked");
+
+        $("#am-container").css("display", "none");
+        $("#hni-container").css("display", "none");
+        $("#ci-container").css("display", "block");
+    });
+})
