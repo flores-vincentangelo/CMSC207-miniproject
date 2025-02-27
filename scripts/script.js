443c7c6be4bb5bdc9337aeb9cce84cd53ced1253
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2025-02-27 15:25:15 +0800

    added animations

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,66 @@
-$(document).ready(function () { 
-    $(".section-buttons").click(function (e) { 
+$(document).ready(function () {
+
+    $("#enter").click(function (e) { 
         e.preventDefault();
+        $(".header-container").css("animation-name", "header-anim");
+        $(".header-container").css("height", "30vh");
+        
+        $("#enter").css("animation-name", "nav-fade-anim");
+        $("#enter").css("display", "none");
+        
+        $(".nav-container").css("animation-name", "nav-fade-anim");
+        $(".nav-container").css("display", "flex")
+
     });
 
-    $("#am-button").click(function (e) { 
-        console.log("clicked");
+    $(".section-buttons").click(function (e) {
+        e.preventDefault();
+        // console.log(this)
+        // console.log(this.id)
 
-        $("#am-container").css("display", "block");
-        $("#hni-container").css("display", "none");
-        $("#ci-container").css("display", "none");
-    });
-    
-    $("#hni-button").click(function (e) { 
-        console.log("clicked");
+        const linkSelectedCss = {
+            "color": "white",
+            "background-color": "var(--orange)",
+            "border": "3px solid, var(--orange)",
+            "border-radius": "15px",
+        }
 
-        $("#am-container").css("display", "none");
-        $("#hni-container").css("display", "block");
-        $("#ci-container").css("display", "none");
-    });
+        const linkClearCss = {
+            "border": "none",
+            "background-color": "transparent",
+            "color": "black"
+        }
 
-    $("#ci-button").click(function (e) { 
-        console.log("clicked");
+        switch (this.id) {
+            case "am-button":
+                $("#am-container").css("display", "block");
+                $("#hni-container").css("display", "none");
+                $("#ci-container").css("display", "none");
 
-        $("#am-container").css("display", "none");
-        $("#hni-container").css("display", "none");
-        $("#ci-container").css("display", "block");
-    });
+                $(".section-buttons").css(linkClearCss);
+                $(`#${this.id}`).css(linkSelectedCss);
+                break;
+            case "hni-button":
+                $("#am-container").css("display", "none");
+                $("#hni-container").css("display", "block");
+                $("#ci-container").css("display", "none");
+                $(".section-buttons").css(linkClearCss);
+                $(`#${this.id}`).css(linkSelectedCss);
+                break;
+            case "ci-button":
+                $("#am-container").css("display", "none");
+                $("#hni-container").css("display", "none");
+                $("#ci-container").css("display", "block");
+                $(".section-buttons").css(linkClearCss);
+                $(`#${this.id}`).css(linkSelectedCss);
+                break;
 
-    $("#fun-fact-btn").click(function (e) { 
+            default:
+                break;
+        }
+    })
+
+    $("#fun-fact-btn").click(function (e) {
         e.preventDefault();
         $("#fun-fact").css("display", "none");
     });
