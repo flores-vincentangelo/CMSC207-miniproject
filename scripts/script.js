f91043de11c97c7810d6fc63879c54e33ee806fe
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2025-02-27 16:44:42 +0800

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 
-    $("#enter").click(function (e) { 
+    $("#enter").click(function (e) {
         e.preventDefault();
         $(".header-container").css("animation-name", "header-anim");
         $(".header-container").css("height", "30vh");
-        
+
         $("#enter").css("animation-name", "nav-fade-anim");
         $("#enter").css("display", "none");
-        
+
         $(".nav-container").css("animation-name", "nav-fade-anim");
         $(".nav-container").css("display", "flex")
 
@@ -15,49 +15,54 @@ $(document).ready(function () {
 
     $(".section-buttons").click(function (e) {
         e.preventDefault();
-        // console.log(this)
-        // console.log(this.id)
 
-        const linkSelectedCss = {
-            "color": "white",
-            "background-color": "var(--orange)",
-            "border": "3px solid, var(--orange)",
-            "border-radius": "15px",
-        }
+        const previousNavItem = $(".selected").attr("id");
+        console.log(previousNavItem);
 
-        const linkClearCss = {
-            "border": "none",
-            "background-color": "transparent",
-            "color": "black"
-        }
+        $(".section-buttons").removeClass('selected');
+        $(`#${this.id}`).addClass('selected');
 
         switch (this.id) {
             case "am-button":
-                $("#am-container").css("display", "block");
+                $("#am-container").css("display", "flex");
                 $("#hni-container").css("display", "none");
                 $("#ci-container").css("display", "none");
-
-                $(".section-buttons").css(linkClearCss);
-                $(`#${this.id}`).css(linkSelectedCss);
                 break;
             case "hni-button":
+                $(`#am-container`).addClass('section-move-away');
+                $(`#hni-container`).addClass('section-move-to');
                 $("#am-container").css("display", "none");
                 $("#hni-container").css("display", "block");
                 $("#ci-container").css("display", "none");
-                $(".section-buttons").css(linkClearCss);
-                $(`#${this.id}`).css(linkSelectedCss);
+
                 break;
             case "ci-button":
                 $("#am-container").css("display", "none");
                 $("#hni-container").css("display", "none");
                 $("#ci-container").css("display", "block");
-                $(".section-buttons").css(linkClearCss);
-                $(`#${this.id}`).css(linkSelectedCss);
                 break;
 
             default:
                 break;
         }
+
+        // switch (previousNavItem) {
+        //     case "am-button":
+        //         $(`#am-container`).addClass('section-move-away');
+
+        //         break;
+        //     case "hni-button":
+
+        //         $(`#hni-container`).addClass('section-move-away');
+        //         break;
+        //     case "ci-button":
+        //         $(`#ci-container`).addClass('section-move-away');
+        //         break;
+
+        //     default:
+        //         break;
+        // }
+
     })
 
     $("#fun-fact-btn").click(function (e) {
