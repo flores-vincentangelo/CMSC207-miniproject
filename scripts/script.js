27309d639725636efced012d63229506a4c44273
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2025-02-28 21:28:56 +0800

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,53 @@
 $(document).ready(function () {
 
-    $("#enter").click(function (e) {
-        e.preventDefault();
-        $(".header-container").css("animation-name", "header-anim");
-        $(".header-container").css("height", "30vh");
+	let isHeaderLarge = true;
 
-        $("#enter").css("animation-name", "nav-fade-anim");
-        $("#enter").css("display", "none");
+	$("#enter").click(function (e) {
+		e.preventDefault();
+		$(".header-container").css("animation-name", "header-anim");
+		$(".header-container").css("height", "30vh");
 
-        $(".nav-container").css("animation-name", "nav-fade-anim");
-        $(".nav-container").css("display", "flex")
-        
-    });
-    
-    $(".title h1").click(function (e) {
-        $(".header-container").css("animation-name", "header-anim-rev");
-        $(".header-container").css("height", "100vh");
-        
-        $("#enter").css("animation-name", "nav-fade-anim-rev");
-        $("#enter").css("display", "block");
-        
-        $(".nav-container").css("animation-name", "nav-fade-anim-rev");
-        $(".nav-container").css("display", "none")
-    })
+		$("#enter").css("animation-name", "nav-fade-anim");
+		$("#enter").css("display", "none");
 
-    $(".section-buttons").click(function (e) {
-        // e.preventDefault();
+		$(".nav-container").css("animation-name", "nav-fade-anim");
+		$(".nav-container").css("display", "flex")
 
-        const previousNavItem = $(".selected").attr("id");
-        console.log(previousNavItem);
+		$(".title h1").css("cursor", "pointer");
 
-        $(".section-buttons").removeClass('selected');
-        $(`#${this.id}`).addClass('selected');
+		isHeaderLarge = false;
 
-    })
+	});
 
-    $("#fun-fact-btn").click(function (e) {
-        e.preventDefault();
-        $("#fun-fact").css("display", "none");
-    });
+	$(".title h1").click(function (e) {
+		if (!isHeaderLarge) {
+			$(".header-container").css("animation-name", "header-anim-rev");
+			$(".header-container").css("height", "100vh");
+
+			$("#enter").css("animation-name", "nav-fade-anim-rev");
+			$("#enter").css("display", "block");
+
+			$(".nav-container").css("animation-name", "nav-fade-anim-rev");
+			$(".nav-container").css("display", "none");
+			$(".title h1").css("cursor", "auto");
+
+			isHeaderLarge = true;
+		}
+	})
+
+	$(".section-buttons").click(function (e) {
+		// e.preventDefault();
+
+		const previousNavItem = $(".selected").attr("id");
+		console.log(previousNavItem);
+
+		$(".section-buttons").removeClass('selected');
+		$(`#${this.id}`).addClass('selected');
+
+	})
+
+	$("#fun-fact-btn").click(function (e) {
+		e.preventDefault();
+		$("#fun-fact").css("display", "block");
+	});
 })
